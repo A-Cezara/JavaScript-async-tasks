b70f715a7e1a392cdd7045550f3850743741bbdd
A-Cezara <alexandracezara@yahoo.ro>
2024-05-22 12:23:40 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const background = document.querySelector('body');
+const startBttn = document.querySelector('[data-start]');
+const stopBttn = document.querySelector('[data-stop]');
+let timerId = null;
 
+
+startBttn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+      background.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  });
+
+stopBttn.addEventListener("click", () => {
+    clearInterval(timerId);
+  });
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
